73fb345b57c5f64b58cc5fd62b354c5d3e66ec9f
redthing1 <xhyinne@gmail.com>
2021-10-02 17:08:18 -0700

    show color

--- src/main.c
+++ src/main.c
@@ -6,5 +6,8 @@ OBJ_AFFINE *obj_aff_buffer = (OBJ_AFFINE *)obj_buffer;
 
 int main()
 {
-    pal_bg_mem[0] = CLR_RED;
+    REG_DISPCNT = DCNT_MODE0;
+    REG_DISPCNT |= DCNT_BG0;
+
+    pal_bg_mem[0] = CLR_BLUE;
 }
